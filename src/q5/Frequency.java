e8e7ba06ea332602227a8dea3bbaf6b3ae0576fd
Emily Lauren Roth <emilylroth@wireless-10-145-159-13.public.utexas.edu>
2018-09-09 18:25:49 -0500

    Initial commit

---
+++ src/q5/Frequency.java
@@ -0,0 +1,20 @@
+package q5;
+
+import java.util.concurrent.Callable;
+
+public class Frequency implements Callable<Integer> {
+    // method to compute the number of times x appears in A
+    public static int parallelFreq(int x, int[] A, int numThreads){
+        //your implementation goes here, return -1 if the input is not valid.
+        // create the threads
+        for(int i = 0; i <numThreads; i++){
+            // make the threads
+        }
+        return 0;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
+}
